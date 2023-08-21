245c2d1648c4a933e55700cd0c56c0b7b40fe00a
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-08-21 20:45:56 +0600

    recap OOP concept

---
+++ typescriptlang.org/interface.ts
@@ -0,0 +1,14 @@
+interface InterfaceProps {
+    id: string;
+    name: string;
+    age: number;
+}
+
+class Student implements InterfaceProps {
+    constructor(public id: string, public name: string, public age: number) {}
+}
+
+// superclass can be used as interface
+class Employee implements Student {
+    constructor(public id: string, public name: string, public age: number) {}
+}
