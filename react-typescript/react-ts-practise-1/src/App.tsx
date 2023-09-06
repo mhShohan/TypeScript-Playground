b0956d0be9ceebe5869b87c9a2a91ac4fbaaffde
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-09-06 23:46:20 +0600

    small change

--- react-typescript/react-ts-practise-1/src/App.tsx
+++ react-typescript/react-ts-practise-1/src/App.tsx
@@ -2,30 +2,30 @@ import Heading, { Typography } from './components/Heading';
 import Person from './components/Person';
 
 const person1 = {
-    name: 'shohan',
-    age: 23,
-    skills: [
-        { id: 1, title: 'js' },
-        { id: 2, title: 'ts' },
-    ],
-    isActive: true,
-    address: {
-        // city: 'pabna',
-        // country: 'bd',
-    },
+  name: 'shohan',
+  age: 23,
+  skills: [
+    { id: 1, title: 'js' },
+    { id: 2, title: 'ts' },
+  ],
+  isActive: true,
+  address: {
+    // city: 'pabna',
+    // country: 'bangladesh',
+  },
 };
 
 const App = () => {
-    return (
-        <div className='app'>
-            <Person {...person1} />
-            <br />
-            <br />
-            <Heading>
-                <Typography>MH Shohan</Typography>
-            </Heading>
-        </div>
-    );
+  return (
+    <div className='app'>
+      <Person {...person1} />
+      <br />
+      <br />
+      <Heading>
+        <Typography>MH Shohan</Typography>
+      </Heading>
+    </div>
+  );
 };
 
 export default App;
